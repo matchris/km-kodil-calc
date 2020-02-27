cce9ebabec844834de27a5c94637223909017b7a
matchris <krzysztof.matyjaszek@outlook.com>
2020-02-27 22:55:11 +0100

    Kalkulator z bledem na porcie 8080

---
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -0,0 +1,34 @@
+package com.kodilla.kalkulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Calculator {
+
+    double a;
+    double b;
+
+    public void addNumbers(double a, double b){
+        System.out.println("Adding two numbers " + a + " and " + b);
+        System.out.println("The sum is: " + a+b);
+    }
+
+    public void substrNumbers(double a, double b){
+        System.out.println("Substract two numbers " + a + " and " + b);
+        System.out.println("The difference is: " + (a-b));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator.class, args);
+    }{
+        a=3444.33;
+        b=2223.11;
+        addNumbers(a,b);
+        substrNumbers(a,b);
+    }
+
+}
+
+
+
+
